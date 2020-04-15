acf092ae164c4de38f72d66adaa853a9f1693f36
z-sprague <32646325+z-sprague@users.noreply.github.com>
2020-04-15 15:24:33 -0400

    Multiple Improvements
    Add the abilty to view records back to 1969.

--- app.py
+++ app.py
@@ -1,16 +1,44 @@
 from flask import Flask, render_template, jsonify, request
 from random import sample
-from functions import alea_data, alct_data, alwt_data, nlea_data, nlct_data, nlwt_data
+from functions import get_data
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def chart():
-    return render_template('index.html',
-                           alea_data=alea_data, alct_data=alct_data,
-                           nlea_data=nlea_data, nlct_data=nlct_data,
-                           alwt_data=alwt_data, nlwt_data=nlwt_data)
+    alct_data = None
+    nlct_data = None
+    years = []
+    for i in range(2019, 1968, -1):
+        years.append(i)
+    if request.args.get('years') is None:
+        year = 2019
+    else:
+        year = int(request.args.get('years'))
+
+    alea_data = get_data(year, 0)
+
+    if year > 1993:
+        alct_data = get_data(year, 1)
+        alwt_data = get_data(year, 2)
+        nlea_data = get_data(year, 3)
+        nlct_data = get_data(year, 4)
+        nlwt_data = get_data(year, 5)
+    else:
+        alwt_data = get_data(year, 1)
+        nlea_data = get_data(year, 2)
+        nlwt_data = get_data(year, 3)
+
+    # alea_data = alea_stats(year)
+    # alwt_data = alwt_stats(year)
+    # nlea_data = nlea_stats(year)
+    # nlwt_data = nlwt_stats(year)
+
+    return render_template('index.html', years=years, year=year,
+                           alea_data=alea_data, nlea_data=nlea_data,
+                           alwt_data=alwt_data, nlwt_data=nlwt_data,
+                           alct_data=alct_data, nlct_data=nlct_data)
 
 
 @app.route('/data')
--- functions.py
+++ functions.py
@@ -1,77 +1,13 @@
 from pybaseball import standings
 
-data = standings(2019)
+# 1994 start of 5 teams per division
 
-def getTeamData(info, index):
-    arr = []
-    for x in data[index].get(info):
-        arr.append(x)
-    return arr
+# data = standings(2019)
 
-# MLB Team Stats for American League East
-def alea_stats():
-    team = getTeamData('Tm', 0)
-    wins = getTeamData('W', 0)
-    loses = getTeamData('L', 0)
-    win_lose = getTeamData('W-L%', 0)
-    gb = getTeamData('GB', 0)
-    alea_data = [team, wins, loses, win_lose, gb]
-    return alea_data
+def get_data(year, div_id):
+    league_data = standings(year)[div_id].values
+    return league_data
 
-# MLB Team Stats for American League Central
-def alct_stats():
-    team = getTeamData('Tm', 1)
-    wins = getTeamData('W', 1)
-    loses = getTeamData('L', 1)
-    win_lose = getTeamData('W-L%', 1)
-    gb = getTeamData('GB', 1)
-    alct_data = [team, wins, loses, win_lose, gb]
-    return alct_data
 
-# MLB Team Stats for American League West
-def alwt_stats():
-    team = getTeamData('Tm', 2)
-    wins = getTeamData('W', 2)
-    loses = getTeamData('L', 2)
-    win_lose = getTeamData('W-L%', 2)
-    gb = getTeamData('GB', 2)
-    alwt_data = [team, wins, loses, win_lose, gb]
-    return alwt_data
 
-# MLB Team Stats for National League East
-def nlea_stats():
-    team = getTeamData('Tm', 3)
-    wins = getTeamData('W', 3)
-    loses = getTeamData('L', 3)
-    win_lose = getTeamData('W-L%', 3)
-    gb = getTeamData('GB', 3)
-    nlea_data = [team, wins, loses, win_lose, gb]
-    return nlea_data
-
-# MLB Team Stats for National League Central
-def nlct_stats():
-    team = getTeamData('Tm', 4)
-    wins = getTeamData('W', 4)
-    loses = getTeamData('L', 4)
-    win_lose = getTeamData('W-L%', 4)
-    gb = getTeamData('GB', 4)
-    nlct_data = [team, wins, loses, win_lose, gb]
-    return nlct_data
-
-# MLB Team Stats for National League West
-def nlwt_stats():
-    team = getTeamData('Tm', 5)
-    wins = getTeamData('W', 5)
-    loses = getTeamData('L', 5)
-    win_lose = getTeamData('W-L%', 5)
-    gb = getTeamData('GB', 5)
-    nlwt_data = [team, wins, loses, win_lose, gb]
-    return nlwt_data
-
-alea_data = alea_stats()
-alct_data = alct_stats()
-alwt_data = alwt_stats()
-nlea_data = nlea_stats()
-nlct_data = nlct_stats()
-nlwt_data = nlwt_stats()
 
