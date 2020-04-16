c735f22417aeb4a808c5c0c92ced05eff5a0032f
z-sprague <32646325+z-sprague@users.noreply.github.com>
2020-04-15 20:40:53 -0400

    More graphs and speed improvement

--- app.py
+++ app.py
@@ -1,44 +1,50 @@
 from flask import Flask, render_template, jsonify, request
 from random import sample
-from functions import get_data
+from functions import get_data, get_size, bar_colors, bar_outline_colors
 
 app = Flask(__name__)
 
 
 @app.route('/', methods=['GET'])
 def chart():
-    alct_data = None
-    nlct_data = None
+    alea_data, nlea_data, alct_data, nlct_data, alwt_data, nlwt_data, older_data, \
+        = None, None, None, None, None, None, None,
+
+    bc = bar_colors
+    boc = bar_outline_colors
+    size = 0
+
     years = []
-    for i in range(2019, 1968, -1):
+    for i in range(2019, 1870, -1):
         years.append(i)
     if request.args.get('years') is None:
         year = 2019
     else:
         year = int(request.args.get('years'))
 
-    alea_data = get_data(year, 0)
+    if year > 1968:
+        alea_data = get_data(year, 0)
 
-    if year > 1993:
-        alct_data = get_data(year, 1)
-        alwt_data = get_data(year, 2)
-        nlea_data = get_data(year, 3)
-        nlct_data = get_data(year, 4)
-        nlwt_data = get_data(year, 5)
+        if year > 1993:
+            alct_data = get_data(year, 1)
+            alwt_data = get_data(year, 2)
+            nlea_data = get_data(year, 3)
+            nlct_data = get_data(year, 4)
+            nlwt_data = get_data(year, 5)
+        else:
+            alwt_data = get_data(year, 1)
+            nlea_data = get_data(year, 2)
+            nlwt_data = get_data(year, 3)
     else:
-        alwt_data = get_data(year, 1)
-        nlea_data = get_data(year, 2)
-        nlwt_data = get_data(year, 3)
-
-    # alea_data = alea_stats(year)
-    # alwt_data = alwt_stats(year)
-    # nlea_data = nlea_stats(year)
-    # nlwt_data = nlwt_stats(year)
+        older_data = get_data(year)
+        size = int(get_size(year))
 
     return render_template('index.html', years=years, year=year,
                            alea_data=alea_data, nlea_data=nlea_data,
                            alwt_data=alwt_data, nlwt_data=nlwt_data,
-                           alct_data=alct_data, nlct_data=nlct_data)
+                           alct_data=alct_data, nlct_data=nlct_data,
+                           older_data=older_data, bc=bc,
+                           boc=boc, size=size)
 
 
 @app.route('/data')
--- functions.py
+++ functions.py
@@ -1,13 +1,24 @@
 from pybaseball import standings
 
+bar_outline_colors = ['rgb(245, 140, 144)','rgb(71, 146, 173)','rgb(30, 122, 60)','rgb(227, 6, 107)','rgb(110, 207, 9)',
+              'rgb(193, 194, 221)','rgb(3, 107, 161)','rgb(125, 165, 217)','rgb(107, 112, 135)','rgb(231, 68, 237)',
+              'rgb(46, 38, 232)','rgb(118, 15, 185)','rgb(77, 169, 185)','rgb(144, 200, 133)','rgb(73, 196, 227)',
+              'rgb(149, 155, 89)','rgb(42, 49, 10)','rgb(107, 72, 87)','rgb(104, 13, 228)','rgb(5, 246, 62)']
+bar_colors = ['rgb(245, 140, 144, .7)','rgb(71, 146, 173, .7)','rgb(30, 122, 60, .7)','rgb(227, 6, 107, .7)','rgb(110, 207, 9, .7)',
+              'rgb(193, 194, 221, .7)','rgb(3, 107, 161, .7)','rgb(125, 165, 217, .7)','rgb(107, 112, 135, .7)','rgb(231, 68, 237, .7)',
+              'rgb(46, 38, 232, .7)','rgb(118, 15, 185, .7)','rgb(77, 169, 185, .7)','rgb(144, 200, 133, .7)','rgb(73, 196, 227, .7)',
+              'rgb(149, 155, 89, .7)','rgb(42, 49, 10, .7)','rgb(107, 72, 87, .7)','rgb(104, 13, 228, .7)','rgb(5, 246, 62, .7)']
+
 # 1994 start of 5 teams per division
+# data = standings(1960)
 
-# data = standings(2019)
-
-def get_data(year, div_id):
+def get_data(year, div_id=0):
     league_data = standings(year)[div_id].values
     return league_data
 
-
+def get_size(year):
+    val = standings(year)
+    size = val[0].get('Name')
+    return size.size
 
 
